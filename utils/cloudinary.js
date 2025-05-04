c1833bed42183dd5798a7d46c88dbaff12f3d994
fsdsfd <tomasmarcos2019@gmail.com>
2025-05-04 16:09:44 -0300

    actualizando variables de entorno

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,15 +1,15 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 console.log('Cloudinary config:', {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
 });
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
 });
 
 export default cloudinary;
