69aab977ff1ee65344d3ca874f3bd05525bee373
fsdsfd <tomasmarcos2019@gmail.com>
2025-05-04 15:25:26 -0300

    arreglo de errores

--- routers/upload.router.js
+++ routers/upload.router.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import productosController from '../controllers/productos.controller.js'
-import upload from '../middlewares/multer-cloudinary.js'
+import controladores from '../controllers/productos.controllers.js'
+import upload from '../middlewares/upload.cloudinary.js'
 
 const router = express.Router()
 
-router.post('/', upload.array('foto', 10), productosController.create)
+router.post('/', upload.array('foto', 10), controladores.create)
 
 export default router
